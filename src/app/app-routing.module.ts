1c78d9defcce913a3acd5cd26497028c4bffc380
Isabel <k.isa02.ig@gmail.com>
2024-10-14 13:50:02 -0500

    Actualizando los archivos del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/users/principal/principal.component';
+import { NosotrosComponent } from './pages/users/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/users/contacto/contacto.component';
+import { GaleriaComponent } from './pages/users/galeria/galeria.component';
+import { DetalleProductoComponent } from './pages/users/detalle-producto/detalle-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'detalle/:id', component: DetalleProductoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NosotrosComponent } from './pages/users/nosotros/nosotros.component';
+import { PrincipalComponent } from './pages/users/principal/principal.component';
+import { ContactoComponent } from './pages/users/contacto/contacto.component';
+import { GaleriaComponent } from './pages/users/galeria/galeria.component';
+import { DetalleProductoComponent } from './pages/users/detalle-producto/detalle-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NosotrosComponent,
+    PrincipalComponent,
+    ContactoComponent,
+    GaleriaComponent,
+    DetalleProductoComponent
   ],
   imports: [
     BrowserModule,
